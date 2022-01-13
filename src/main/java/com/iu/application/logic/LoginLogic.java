b20b8b2c583d346fb1a71ffcd0f3e13f08ca3553
Tagliagole <domila98@hotmail.de>
2022-01-13 16:59:07 +0100

    LgonView erstellt

---
+++ src/main/java/com/iu/application/logic/LoginLogic.java
@@ -0,0 +1,9 @@
+package com.iu.application.logic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginLogic {
+
+}
