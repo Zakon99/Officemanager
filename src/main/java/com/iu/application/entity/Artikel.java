d5aac9817d841db46e19c4670084d2fae8194265
Zakon99 <88721691+Zakon99@users.noreply.github.com>
2022-01-14 16:23:54 +0100

    Abschreibung hinzugefügt
    Logik für die Erstellung einer Abschreibung eines Artikels wurde hinzugefügt.

---
+++ src/main/java/com/iu/application/entity/Artikel.java
@@ -0,0 +1,63 @@
+package com.iu.application.entity;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class Artikel {
+    private Long userID;
+    private String name;
+    private int anzahl;
+    private int preis;
+    private LocalDate kaufDatum;
+
+    public Artikel(Long userID, String name, int anzahl, int preis, LocalDate kaufDatum) {
+        this.userID = userID;
+        this.name = name;
+        this.anzahl = anzahl;
+        this.preis = preis;
+        this.kaufDatum = kaufDatum;
+    }
+
+    public void setKaufDatum(LocalDate kaufDatum) {
+        this.kaufDatum = kaufDatum;
+    }
+
+    //Getter&Setter
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAnzahl() {
+        return anzahl;
+    }
+
+    public void setAnzahl(int anzahl) {
+        this.anzahl = anzahl;
+    }
+
+    public LocalDate getKaufDatum() {
+        return kaufDatum;
+    }
+
+    public int getPreis() {
+        return preis;
+    }
+
+    public void setPreis(int preis) {
+        this.preis = preis;
+    }
+}
